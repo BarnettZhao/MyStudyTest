1f06496d37e949b9ac5c19fc71b743938a2963a2
Z <201910>
2019-10-12 15:06:02 +0800

    修改图片加载类的警告

--- app/src/main/java/com/example/myapplication/PictureLoader.java
+++ app/src/main/java/com/example/myapplication/PictureLoader.java
@@ -14,12 +14,12 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class PictureLoader {
+class PictureLoader {
     private ImageView loadImg;
     private String imgUrl;
     private byte[] picByte;
 
-    Handler handler = new Handler() {
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
@@ -32,20 +32,20 @@ public class PictureLoader {
         }
     };
 
-    public void load(ImageView loadImg, String imgUrl) {
+    void load(ImageView loadImg, String imgUrl) {
         this.loadImg = loadImg;
         this.imgUrl = imgUrl;
         Drawable drawable = loadImg.getDrawable();
-        if(drawable != null && drawable instanceof BitmapDrawable) {
-            Bitmap bitmap = ((BitmapDrawable)drawable).getBitmap();
-            if(bitmap != null && !bitmap.isRecycled()) {
+        if (drawable instanceof BitmapDrawable) {
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            if (bitmap != null && !bitmap.isRecycled()) {
                 bitmap.recycle();
             }
         }
         new Thread(runnable).start();
     }
 
-    Runnable runnable = new Runnable() {
+    private Runnable runnable = new Runnable() {
         @Override
         public void run() {
             try {
@@ -57,7 +57,7 @@ public class PictureLoader {
                     InputStream in = conn.getInputStream();
                     ByteArrayOutputStream out = new ByteArrayOutputStream();
                     byte[] bytes = new byte[1024];
-                    int length = -1;
+                    int length;
                     while ((length = in.read(bytes)) != -1) {
                         out.write(bytes, 0, length);
                     }
